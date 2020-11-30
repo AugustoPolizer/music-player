853a8243ba5d730374fb12609f0199e42984b556
PolizerBruno <60673615+PolizerBruno@users.noreply.github.com>
2020-11-30 17:21:38 -0300

    library with images

--- src/components/Player/Library.tsx
+++ src/components/Player/Library.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
-import { ScrollView, TextInput, TouchableOpacity } from "react-native-gesture-handler";
+import { Text, StyleSheet, Image, View } from "react-native";
+import {
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native-gesture-handler";
 import { Music } from "../../types/commons";
 
 export type Props = {
@@ -10,34 +14,61 @@ export type Props = {
 const PlayerButton: React.FC<Props> = (props) => {
   return (
     <ScrollView>
-      <TextInput style={styles.searchInput} placeholder={'Search'}></TextInput>
+      <TextInput style={styles.searchInput} placeholder={"Search"}></TextInput>
+      <View style={styles.libraryMusic}>
       {props.musics.map((music) => {
         return (
-          <TouchableOpacity key={music.name + music.duration}>
-            <Text style={styles.libraryText}>{String(music.name).substring(0,String(music.name).lastIndexOf('.'))}</Text>
+          <TouchableOpacity style={styles.displayer} key={music.name + music.duration}>
+            <Image
+              style={styles.tinyLogo}
+              source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }}
+            />
+            <Text style={styles.libraryText}>
+              {String(music.name).substring(
+                0,
+                String(music.name).lastIndexOf(".")
+              )}
+            </Text>
           </TouchableOpacity>
         );
       })}
+      </View>
+     
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-
   libraryText: {
     textAlign: "center",
     textTransform: "capitalize",
     fontSize: 18,
     color: "black",
-    padding : 10,
+    padding: 10,
   },
-  searchInput : {
-    width : '95%',
-    height : 40,
-    textAlign : 'center',
-    borderWidth : 2,
-    borderRadius : 20,
+  searchInput: {
+    width: "95%",
+    height: 40,
+    textAlign: "center",
+    borderWidth: 2,
+    borderRadius: 20,
+    margin: 10,
+  },
+  tinyLogo: {
+    width: 100,
+    height: 100,
+  },
+  displayer: {
+    padding : 10,
     margin : 10,
+  },
+  libraryMusic : {
+    flex : 1,
+    alignItems : 'center',
+    justifyContent : 'center',
+    flexDirection : 'row',
+    flexWrap : 'wrap',
+  
   }
 });
 
