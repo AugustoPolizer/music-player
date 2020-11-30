ba018a2e4e66c2647cad05ed4c4571750e4c72c5
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-11-29 23:11:54 -0300

    Removendo o music context

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,10 +1,5 @@
-import React from 'react'
 import Home from './Home';
-import { Music } from '../types/commons';
-
-const musicContext = React.createContext<Array<Music>>([]);
 
 export {
   Home,
-  musicContext
 };
